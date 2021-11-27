2ff310dad5a412c5a7784c88255092f95ab5bbf3
ruchirjain24 <62206895+ruchirjain24@users.noreply.github.com>
2021-11-27 07:17:46 +0530

    Delete verilog files directory

--- verilog files/helloworld.v
+++
@@ -1,8 +0,0 @@
-//https://drive.google.com/file/d/1axWmpz-saQrbXKGRZL8bU9B34uWZ7vvR/view
-module hello;
- initial
- begin
- $display("Hello World");
- $finish ;
- end
-endmodule
--- verilog files/mux2to1_df.v
+++
@@ -1,5 +0,0 @@
-module mux2to1_df (a,b,s,f);
-    input a,b,s;
-    output f;
-    assign f=s?a:b;
-endmodule
